dc36c253d368bd5c12a1f706e6be62b3bdab7818
AmalDer <amalderouich00@gmail.com>
2022-06-29 16:46:29 +0100

    first commit

---
+++ frontendup/src/components/Groups.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Groups(){
+    return(
+        <section>
+            <div>
+                <h1>La liste des groupes</h1>
+            </div>
+        </section>
+    );
+
+}
+
+export default Groups;
